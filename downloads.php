e01c52df35d3cd25bc3b5d66fb5273866a25e09f
SnowdenWintermute <michael.p.silverman@gmail.com>
2018-10-24 14:57:49 -0400

    removed menu blur

--- downloads.php
+++ downloads.php
@@ -7,43 +7,9 @@
             DOWNLOADS
         </h1>    
     </header>
-    <h3>IMAGES</h3>
+
     <section id="img-downloads">
-        <div class="download-img-holder">
-            <img src="./downloads/img/surreal-book.png" alt="">
-            <div class="download-img-links-holder">
-                <a href="./downloads/img/surreal-book.png" target="_blank">View</a>
-                <a href="./downloads/img/surreal-book.png" download>Download</a>
-            </div>
-        </div>
-        <div class="download-img-holder">
-            <img src="./downloads/img/surreal-cup.jpg" alt="">
-            <div class="download-img-links-holder">
-                <a href="./downloads/img/surreal-cup.jpg" target="_blank">View</a>
-                <a href="./downloads/img/surreal-cup.jpg" download>Download</a>
-            </div>
-        </div>
-        <div class="download-img-holder">
-            <img src="./downloads/img/planets.png" alt="">
-            <div class="download-img-links-holder">
-                <a href="./downloads/img/planets.png" target="_blank">View</a>
-                <a href="./downloads/img/planets.png" download>Download</a>
-            </div>
-        </div>
-        <div class="download-img-holder">
-            <img src="./downloads/img/mushroom.jpg" alt="">
-            <div class="download-img-links-holder">
-                <a href="./downloads/img/mushroom.jpg" target="_blank">View</a>
-                <a href="./downloads/img/mushroom.jpg" download>Download</a>
-            </div>
-        </div>
-        <div class="download-img-holder">
-            <img src="./downloads/img/chest.jpg" alt="">
-            <div class="download-img-links-holder">
-                <a href="./downloads/img/chest.jpg" target="_blank">View</a>
-                <a href="./downloads/img/chest.jpg" download>Download</a>
-            </div>
-        </div>
+        Coming soon...
     </section>
     </main>
 <script src="./js/index.js"></script>
