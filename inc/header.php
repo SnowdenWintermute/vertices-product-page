80b3bb9211ce585ee777ab44b3a5c100fab67747
Mike <michael.p.silverman@gmail.com>
2023-08-28 12:35:38 -0400

    changed from php to html for github pages hosting

--- inc/header.php
+++ inc/header.php
@@ -11,4 +11,25 @@
     <link rel="shortcut icon" href="./favicon.ico" />
 </head>
 <body>
-<?php include('nav.php') ?>
+<nav id="topNav">
+    <div id="nav-links-holder">
+    <ul class="navLinks" id="navLinksLeft">
+        <li><a href="about.php">About</a>
+        </li>
+        <li><a href="projects.php">Projects</a>
+        </li>
+    </ul>
+    <div id="brand">
+        <a href="index.php">
+        <img src="./img/brand.png" alt="">
+        </a>
+    </div>
+    <ul class="navLinks" id="navLinksRight">
+        <li><a href="contact.php">Contact</a>
+        </li>
+        <li><a href="downloads.php">Downloads</a>
+        </li>
+    </ul>
+    <div id="hamburger" class="hidden"><i class="fa fa-bars"></i></div>
+    </div>
+</nav>
